8e6ba72759774820d33926490908a80dea53e90d
pyotarou <fujigaki@sec.inf.shizuoka.ac.jp>
2022-08-14 17:58:28 +0900

    change migrate

--- main.go
+++ main.go
@@ -10,11 +10,14 @@ import (
 	"os/signal"
 	"syscall"
 
-	_ "github.com/pyotarou/simplebank/doc/statik"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/pyotarou/simplebank/api"
 	db "github.com/pyotarou/simplebank/db/sqlc"
+	_ "github.com/pyotarou/simplebank/doc/statik"
 	"github.com/pyotarou/simplebank/gapi"
 	"github.com/pyotarou/simplebank/pb"
 	"github.com/pyotarou/simplebank/util"
@@ -34,9 +37,24 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewStore(conn)
 	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance: ", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up:", err)
+	}
+
+	log.Println("db migration successfully")
 }
 
 func runGrpcServer(config util.Config, store db.Store) {
